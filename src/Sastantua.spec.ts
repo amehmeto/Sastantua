9378ec45c0c6928eb2ab9e1550edcf611a722e50
amehmeto <42amehmeto@gmail.com>
2020-04-16 19:28:17 +0200

    :white_check_mark: Passing first test with Sastantua size 1

--- src/Sastantua.spec.ts
+++ src/Sastantua.spec.ts
@@ -2,17 +2,29 @@ import {Sastantua} from './Sastantua'
 
 describe('Sastantua Suite Test', () => {
     describe('Sastantua size 1', () => {
-        it('should draw the first stage', () => {
+        it('should draw the first flour no door', () => {
             const pyramid =
                 '  /*\\\n' +
                 ' /***\\\n' +
-                '/**|**\\\n'
+                '/*****\\\n'
             expect(new Sastantua().draw(1)).toStrictEqual(pyramid)
         })
     })
 
+    describe('Sastantua size 2 temporary', () => {
+        it('should draw 2 flours', () => {
+            const pyramid =
+                '        /*\\\n' +
+                '       /***\\\n' +
+                '      /*****\\\n'
+            expect(new Sastantua().draw(2)).toStrictEqual(pyramid)
+        })
+    })
+
+
+    /*
     describe('Sastantua size 2', () => {
-        it.each([
+        it.skip.each([
             [
                 '  /*\\\n' +
                 ' /***\\\n' +
@@ -31,7 +43,6 @@ describe('Sastantua Suite Test', () => {
                 ' /***************\\\n' +
                 '/********|********\\\n'
             ],
-            /*
             [
                 '        /*\\\n' +
                 '       /***\\\n' +
@@ -41,13 +52,11 @@ describe('Sastantua Suite Test', () => {
                 ' /***************\\\n' +
                 '/**|**\\\n'
             ],
-             */
         ])('shoud draw temporary goal', (pyramid) => {
             expect(new Sastantua().draw(2))
                 .toStrictEqual(pyramid)
         })
 
-        /*
         it.skip('shoud draw the first two stages', () => {
             const pyramid =
                 '        /*\\\n' +
@@ -59,7 +68,7 @@ describe('Sastantua Suite Test', () => {
                 '/********|********\\\n'
             expect(new Sastantua().draw(2)).toStrictEqual(pyramid)
         })
-
-         */
     })
+
+     */
 })
--- src/Sastantua.ts
+++ src/Sastantua.ts
@@ -5,49 +5,31 @@ export class Sastantua {
     private BRICK: string = '*'
     private SPACE: string = ' '
     private DOOR: string = '|'
-    private firstTopHeight: number = 3
+    private FIRST_TOP_HEIGHT: number = 3
     private bottomStageLines: number = 1
+    private EMPTY_STRING = ''
 
     draw(size: number): string {
-        if (size === 1)
-            return this.generateFirstFloor(this.firstTopHeight, size)
-        return this.generateTopFloors(1) +
-            this.generateFirstFloor(this.firstTopHeight + 1, size)
+        return this.generateFloors(this.FIRST_TOP_HEIGHT)
     }
 
-    private generateFirstFloor(height: number, size: number) {
-        return this.generateRegularLines(
-                size,
-                height - this.bottomStageLines,
-                ''
-            ) + this.generateDoorLines()
+    private generateFloors(size: number) {
+        return this.generateRegularLines(size)
     }
 
-    private generateTopFloors(size: number) {
-        return this.generateRegularLines(size, this.firstTopHeight, '')
-    }
+    private generateRegularLines(size: number) {
+        let lines = this.EMPTY_STRING
+        let height = this.FIRST_TOP_HEIGHT
+        for (let line = 0 ; height ; height--) {
+            let neededElements = {
+                bricks: 1 + (line++ - 1),
+                spaces: height - 1,
+                door: 0
+            }
 
-    private generateRegularLines(size: number, lastLine: number, accumulator: string): string {
-        let neededElement: Elements = {
-            bricks: 0, spaces: 0, door: 0
+            lines += this.generateLine(LineType.REGULAR, neededElements)
         }
-        if (size === 1){
-            neededElement.bricks = lastLine - 1
-            neededElement.spaces = this.firstTopHeight - lastLine
-            neededElement.door = 0
-        } else {
-            neededElement.bricks = lastLine + size + 2
-            neededElement.spaces = this.firstTopHeight - lastLine + 1
-            neededElement.door = 0
-        }
-        if (lastLine === 0)
-            return accumulator
-        return this.generateRegularLines(
-            size,
-            lastLine - 1,
-            this.generateLine(LineType.REGULAR, neededElement
-            ) + accumulator
-        )
+        return lines
     }
 
     private generateDoorLines() {
